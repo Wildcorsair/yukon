3cfbc40aa14b8d312d67b06d2ebb36b6b2870c14
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2018-10-15 21:41:20 +0300

    Added HTTP codes list for 'Response'

--- core/App/Response.php
+++ core/App/Response.php
@@ -4,12 +4,27 @@ namespace Yukon\Core\App;
 use Yukon\Core\Contracts\HTTPResponse;
 
 /**
- *
+ * Class implements finctionality for HTTP response.
  */
 class Response implements HTTPResponse
 {
+
+    public static $codes = [
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        400 => 'Bad Request',
+        401 => 'Unathorized',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        500 => 'Internal Server Error',
+        502 => 'Bad Gateway'
+    ];
+
     public function __construct($status = 200) {
-        header("HTTP/1.0 {$status}");
+        if (array_key_exists($status, self::$codes)) {
+            header("HTTP/1.1 {$status} " . self::$codes[$status]);
+        }
     }
 
     public function header($key, $value = null)
@@ -21,5 +36,10 @@ class Response implements HTTPResponse
     public function json($data)
     {
         echo json_encode($data);
+    }
+
+    public function content($data)
+    {
+        echo htmlspecialchars($data);
     }
 }
